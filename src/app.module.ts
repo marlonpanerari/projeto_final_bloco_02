d56ffde63ae741ab809b68bf89d223558f11ed69
Marlon Panerari <marpan.dev@gmail.com>
2026-04-16 10:57:40 -0300

    configuração inicial do projeto

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_drugstore',
+      entities: [],
+      synchronize: true,
+    }),
+
+  ],
+})
+export class AppModule {}
